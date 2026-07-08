203eb990ed7d7350cb27020961abff7a78d2839a
phalla-develop07 <suongphalla36@gmail.com>
2026-07-09 06:52:05 +0700

    updated portfolio icon title

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import i18n from "./i18n";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -15,5 +16,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
